6db281915736a988855f841ca5c781816c09cd16
mirgags <markmiraglia@gmail.com>
2015-08-23 16:52:14 -0700

    built users method

--- app.py
+++ app.py
@@ -12,11 +12,32 @@ def getConfig():
 
 def getUsers():
     config = getConfig()
-    data = {}
-    headers = {
-        "Contant-Type": "application/x-www-form-urlencoded",
+    data = {
+        "pagesize":"100",
+        "order":"desc",
+        "sort":"reputation",
+        "site":"stackoverflow"
         }
-    jsonRes = requests.post("http://stackoverflow.com/")
+    headers = {
+        "access_token": config['token'],
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept-Encoding":"gzip"
+        }
+    jsonRes = requests.get("https://api.stackexchange.com/2.2/users",
+        headers=headers,
+        data=data
+    )
+    print jsonRes.headers
+    print jsonRes.text
+    return jsonRes
 
 if __name__ == "__main__":
-    print json.dumps(getConfig());
+    r = getUsers()
+    users = json.loads(r.text)
+    for user in users['items']:
+        print user['display_name']
+        try:
+            print user['location']
+        except:
+            print 'no location'
+        print '*****'
